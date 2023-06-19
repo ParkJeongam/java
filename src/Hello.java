54ffc8160daf96aca9d6e5d0903a321c3ca02859
DESKTOP-FQ8HFRN\user <csad123@naver.com>
2023-06-19 15:00:47 +0900

    fourth commit

--- src/Hello.java
+++ src/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("아행행!! 하오dk");
+		System.out.println("아행행!! 하오dk");
 	}
 
 }
